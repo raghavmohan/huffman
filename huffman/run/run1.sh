1cd0e7520f8923491db90b5c8180ca32eceedeef
Raghav Mohan <raghav.mohan@gmail.com>
2013-04-22 21:42:38 -0500

    before change to add all files to sh script, cpp code

--- huffman/run/run1.sh
+++ huffman/run/run1.sh
@@ -1,10 +1,25 @@
 #! /bin/sh
+LIST=../input/list/
+UNIQUE=../input/uniquewords.txt
+RUN=../output/
+OUTPUT=../output/
+INPUT=../input/
+SPEECH="${INPUT}speechdata/"
+#./unique.sh
+cd ../src
+make huff
+nums[0]=1
+nums[1]=2
+nums[2]=10
+nums[3]=100
+nums[4]=300
 
-cd src
-make clean
-make
-rm -f *.txt
-./huff -i ../input/jefferson-1806.txt -o jefferson.txt
-./huff -i ../input/obama-2009.txt -o obama.txt
-
-
+for i in {0..4}
+do
+  ls ../input/speechdata/*.txt | tail -n ${nums[i]} > ../input/list/recent-${nums[i]}.list
+  ls -r ../input/speechdata/*.txt | tail -n ${nums[i]} > ../input/list/old-${nums[i]}.list
+done
+for i in $(basename ${SPEECH}*.txt); do
+  echo $i
+  #./huff -b ../input/uniquewords.txt -i ../input/list/files.list -o ../output/$i.huff
+done
